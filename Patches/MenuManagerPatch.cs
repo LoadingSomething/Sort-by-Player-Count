5c65d9c028cc473110506a379455d6135054eeef
LoadingSomething <143251849+LoadingSomething@users.noreply.github.com>
2023-12-06 14:42:36 -0800

    Add files via upload

---
+++ Patches/MenuManagerPatch.cs
@@ -0,0 +1,32 @@
+﻿using HarmonyLib;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SortByPlayersButton.Patches {
+    [HarmonyPatch]
+    internal class MenuManagerPatch {
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(MenuManager), "Start")]
+        private static void AddSortButton() {
+            GameObject refreshButtonObject = GameObject.Find("/Canvas/MenuContainer/LobbyList/ListPanel/RefreshButton");
+            if (refreshButtonObject != null) {
+                GameObject sortButtonObject = Object.Instantiate(refreshButtonObject.gameObject, refreshButtonObject.transform.parent);
+                sortButtonObject.GetComponent<RectTransform>().anchoredPosition += new Vector2(0f, 18f);
+                sortButtonObject.GetComponentInChildren<TextMeshProUGUI>().text = "[ Sort ]";
+                Button sortButton = sortButtonObject.GetComponent<Button>();
+                sortButton.onClick = new Button.ButtonClickedEvent();
+                sortButton.onClick.AddListener(() => {
+                    LobbySlot[] lobbySlots = Object.FindObjectsOfType<LobbySlot>();
+                    lobbySlots = lobbySlots.OrderByDescending(lobby => int.Parse(lobby.playerCount.text.Split(' ')[0])).ToArray();
+                    float lobbySlotPositionOffset = 0f;
+                    for (int i = 0; i < lobbySlots.Length; i++) {
+                        lobbySlots[i].gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector2(0f, lobbySlotPositionOffset);
+                        lobbySlotPositionOffset -= 42f;
+                    }
+                });
+            }
+        }
+    }
+}
---
+++ SortByPlayersButtonPlugin.cs
@@ -0,0 +1,17 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System.Reflection;
+
+namespace SortByPlayersButton {
+    [BepInPlugin(modGUID, modName, modVer)]
+    public class SortByPlayersButtonPlugin : BaseUnityPlugin {
+        private const string modGUID = "loadingsomething.SortByPlayersButton";
+        private const string modName = "SortByPlayersButton";
+        private const string modVer = "1.0.0";
+
+        private void Awake() {
+            Harmony harmony = new Harmony(modGUID);
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
